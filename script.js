cf514a240b81bef09cd3d9efaed0304483bce3be
fluxlt <artisdaivoskinas@gmail.com>
2026-02-12 20:03:46 +0200

    Update script.js

--- script.js
+++ script.js
@@ -38,7 +38,6 @@ async function getPlayers() {
   }
 }
 
-// reveal on scroll
 const io = new IntersectionObserver((entries) => {
   entries.forEach(e => { if (e.isIntersecting) e.target.classList.add("in"); });
 }, { threshold: 0.12 });
